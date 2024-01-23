2117b4ee0f5d83f7428b1c8bf99ca03c461c6f55
Johannes Eschricht <johannes@nortic.se>
2024-01-24 00:01:22 +0100

    Add github action for tests

--- packages/newsletter-subscription-js/tests/example.spec.ts
+++ packages/newsletter-subscription-js/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:5173')
+  await page.goto('/')
 })
 
 test.describe('Subscription form', () => {
