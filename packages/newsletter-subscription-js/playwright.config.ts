a77904b4abbd80ac50cb30bc5647f92450b33b32
Johannes Eschricht <johannes@nortic.se>
2024-01-24 00:44:50 +0100

    Add github action for tests (#2)
    * Add github action for tests
    
    * Move playwright package to root dependencies
    
    * Use localhost instead of ip in tests

--- packages/newsletter-subscription-js/playwright.config.ts
+++ packages/newsletter-subscription-js/playwright.config.ts
@@ -21,11 +21,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'list',
+  reporter: [['dot'], ['html', { outputFolder: '../../playwright-report' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'http://127.0.0.1:5174',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -65,9 +65,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'vite --port 5174',
+    url: 'http://127.0.0.1:5174',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
+  },
 })
