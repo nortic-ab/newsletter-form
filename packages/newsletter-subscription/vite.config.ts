276d91556a6bc6664b99ade932d00b0fb670097d
Johannes Eschricht <johannes@nortic.se>
2024-04-18 10:01:41 +0200

    fix: tsconfig issues

--- packages/newsletter-subscription/vite.config.ts
+++ packages/newsletter-subscription/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [svelte(), dts()],
   build: {
     lib: {
-      entry: 'src/main.ts',
+      entry: 'src/index.ts',
       name: 'NorticNewsletter',
       fileName: (module) => {
         switch (module) {
