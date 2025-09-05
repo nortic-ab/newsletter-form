eb4c6cee3fb3b8ad98d2bdc462033225f81f6ef5
Johannes Eschricht <johannes@nortic.se>
2025-09-05 11:09:30 +0200

    fix: Remove test script initialization

--- packages/newsletter-subscription/src/main.ts
+++ packages/newsletter-subscription/src/main.ts
@@ -3,8 +3,3 @@
 import { EmbeddedSubscriptionForm } from '.'
 
 window.EmbeddedSubscriptionForm = EmbeddedSubscriptionForm || {}
-
-const test = new window.EmbeddedSubscriptionForm('#newsletter-form', {
-  hideAffiliation: false,
-  newsletterId: 'test-newsletter',
-})
